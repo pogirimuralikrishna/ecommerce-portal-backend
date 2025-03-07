ceab538bc58470dede29c68653c13375972ce2b4
Murali Krishna <138997008+pogirimuralikrishna@users.noreply.github.com>
2025-03-07 12:23:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const connectDb = require('./db.js')
+const router  = require('./routes/userRoutes.js')
+const app = express()
 
+dotenv.config()
+const PORT = process.env.PORT || 6000
+
+app.use(express.json())
+app.use(cors())
+
+// Connect Database
+connectDb()
+
+app.use('/owner', router)
+
+// Handle routes
+app.listen(PORT, () => {
+    console.log('server is running')
+})
